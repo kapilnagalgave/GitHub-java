d4b071fb8ef844333a5484406803dbced58b9d3b
kapilnagalgave <149675814+kapilnagalgave@users.noreply.github.com>
2024-02-15 12:46:06 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class first {
     public static void main( String[] args)
     {
-        System.out.println("hello my name is kapil and am a java developer");
+        System.out.println("hello my name is kapil and i am a java developer");
     }
 }
