7381675d3fa80ac8cc0f802c7b9de5a05895c73f
kapilnagalgave <149675814+kapilnagalgave@users.noreply.github.com>
2024-05-03 13:16:30 +0530

    Add files via upload
    i learned too take input from user in Java

---
+++ userinput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class userinput {
+         public static void main(String[] args)
+         {
+             Scanner s= new Scanner(System.in); 
+
+        //    int num1; 
+        //     System.out.println("enter the value of the num1");
+        //    num1= s.nextInt();
+        //    System.out.println("the value of num1 is "+num1);
+           System.out.println("enter the string :");
+           String st= s.nextLine();
+           System.out.println(st);
+
+         s.close();
+        }
+       
+    
+    
+
+;
+        }
